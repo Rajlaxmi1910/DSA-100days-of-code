b8e0a5ee1899bef8293bcf0667bb85d6fac6a807
Rajlaxmi1910 <rajlaxmichouhan081@gmail.com>
2026-04-03 23:20:02 +0530

    Add files via upload

---
+++ 100 days of code/question 62.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* next;
+};
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = v;
+    newNode->next = NULL;
+    return newNode;
+}
+void addEdge(struct Node* adj[], int u, int v) {
+    struct Node* newNode = createNode(v);
+    newNode->next = adj[u];
+    adj[u] = newNode;
+    newNode = createNode(u);
+    newNode->next = adj[v];
+    adj[v] = newNode;
+}
+void printGraph(struct Node* adj[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("Vertex %d: ", i);
+        struct Node* temp = adj[i];
+        while (temp) {
+            printf("%d -> ", temp->data);
+            temp = temp->next;
+        }
+        printf("NULL\n");
+    }
+}
+
+int main() {
+    int n, m;
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+    printf("Enter number of edges: ");
+    scanf("%d", &m);
+    struct Node* adj[n];
+    for (int i = 0; i < n; i++)
+        adj[i] = NULL;
+
+    printf("Enter edges (u v):\n");
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        addEdge(adj, u, v);
+    }
+
+    printf("\nAdjacency List:\n");
+    printGraph(adj, n);
+
+    return 0;
+}
