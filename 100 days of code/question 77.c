e1afc8c616a9ff0d386069cbeb39bb551691c762
Rajlaxmi1910 <rajlaxmichouhan081@gmail.com>
2026-04-18 23:34:33 +0530

    Add files via upload

---
+++ 100 days of code/question 77.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+#define MAX 100
+
+int adj[MAX][MAX];
+int visited[MAX];
+int n, m;
+
+void dfs(int node) {
+    visited[node] = 1;
+
+    for (int i = 1; i <= n; i++) {
+        if (adj[node][i] == 1 && visited[i] == 0) {
+            dfs(i);
+        }
+    }
+}
+
+int main() {
+    int u, v;
+
+    // Input
+    scanf("%d %d", &n, &m);
+
+    // Initialize adjacency matrix
+    for (int i = 1; i <= n; i++) {
+        visited[i] = 0;
+        for (int j = 1; j <= n; j++) {
+            adj[i][j] = 0;
+        }
+    }
+
+    // Read edges
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+        adj[v][u] = 1; // undirected graph
+    }
+
+    // Start DFS from node 1
+    dfs(1);
+
+    // Count visited nodes
+    int count = 0;
+    for (int i = 1; i <= n; i++) {
+        if (visited[i] == 1) {
+            count++;
+        }
+    }
+
+    // Check connectivity
+    if (count == n) {
+        printf("CONNECTED\n");
+    } else {
+        printf("NOT CONNECTED\n");
+    }
+
+    return 0;
+}
