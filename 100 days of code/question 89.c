93ec40190fefbb6dd25aed5b42da2733f52747a7
Rajlaxmi1910 <rajlaxmichouhan081@gmail.com>
2026-04-30 18:25:05 +0530

    Add files via upload

---
+++ 100 days of code/question 89.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+int isPossible(int arr[], int n, int m, int maxPages) {
+    int students = 1;
+    int pagesSum = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > maxPages)
+            return 0;
+
+        if (pagesSum + arr[i] > maxPages) {
+            students++;
+            pagesSum = arr[i];
+
+            if (students > m)
+                return 0;
+        } else {
+            pagesSum += arr[i];
+        }
+    }
+    return 1;
+}
+
+int allocateBooks(int arr[], int n, int m) {
+    if (m > n)
+        return -1;
+
+    int low = arr[0], high = 0, result;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > low)
+            low = arr[i];
+        high += arr[i];
+    }
+
+    result = high;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (isPossible(arr, n, m, mid)) {
+            result = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("%d\n", allocateBooks(arr, n, m));
+
+    return 0;
+}
